ca553a73d26fa166920d757b648b7f58632b8382
is12t <love.ms.is12t@hotmail.co.jp>
2022-01-03 03:33:36 +0900

    add hostextensions

---
+++ src/ArgumentsBuilder.Test/UnitTest2.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NUnit.Framework;
+using Rksoftware.ArgumentsBuilder.Attributes;
+using Rksoftware.ArgumentsBuilder.Hosts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArgumentsBuilder.Test;
+
+internal class UnitTest2
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    record struct Arguments([Parameter] string? Aparam, [Parameter] string? Bparam, string? A, string? B, string? C, bool D, bool E) { }
+
+    interface IService { }
+
+    class PublicService : IService
+    {
+        public string Run(Arguments arguments) => arguments.ToString();
+    }
+    class NonPublicService : IService
+    {
+        string Run(Arguments arguments) => arguments.ToString();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var args = new[] {
+            "aparam",
+            "-a",
+            "aopt",
+            "-d",
+            "-b",
+            "bopt",
+            "bparam",
+            "/c",
+            "copt",
+            };
+        using IHost host = Host.CreateDefaultBuilder(args).ConfigureServices((_, services) => services.AddSingleton<IService, PublicService>()).Build();
+        var rsult = host.ApplicationRun<IService, Arguments>(args);
+        Assert.AreEqual(rsult, "Arguments { Aparam = aparam, Bparam = bparam, A = aopt, B = bopt, C = copt, D = True, E = False }");
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var args = new[] {
+            "aparam",
+            "-a",
+            "aopt",
+            "-d",
+            "-b",
+            "bopt",
+            "bparam",
+            "/c",
+            "copt",
+            };
+        using IHost host = Host.CreateDefaultBuilder(args).ConfigureServices((_, services) => services.AddSingleton<IService, NonPublicService>()).Build();
+        var rsult = host.ApplicationRun<IService, Arguments>(args);
+        Assert.AreEqual(rsult, "Arguments { Aparam = aparam, Bparam = bparam, A = aopt, B = bopt, C = copt, D = True, E = False }");
+    }
+
+    [Test]
+    public void Test3()
+    {
+        var args = new[] {
+            "aparam",
+            "-a",
+            "aopt",
+            "-d",
+            "-b",
+            "bopt",
+            "bparam",
+            "/c",
+            "copt",
+            };
+        using IHost host = Host.CreateDefaultBuilder(args).ConfigureServices((_, services) => services.AddSingleton<IService, PublicService>()).Build();
+        var rsult = host.ApplicationRun<IService, Arguments>(args, "run");
+        Assert.AreEqual(rsult, "Arguments { Aparam = aparam, Bparam = bparam, A = aopt, B = bopt, C = copt, D = True, E = False }");
+    }
+
+    [Test]
+    public void Test4()
+    {
+        var args = new[] {
+            "aparam",
+            "-a",
+            "aopt",
+            "-d",
+            "-b",
+            "bopt",
+            "bparam",
+            "/c",
+            "copt",
+            };
+        using IHost host = Host.CreateDefaultBuilder(args).ConfigureServices((_, services) => services.AddSingleton<IService, NonPublicService>()).Build();
+        var rsult = host.ApplicationRun<IService, Arguments>(args, "run");
+        Assert.AreEqual(rsult, "Arguments { Aparam = aparam, Bparam = bparam, A = aopt, B = bopt, C = copt, D = True, E = False }");
+    }
+
+}
---
+++ src/ArgumentsBuilder/Hosts/HostExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rksoftware.ArgumentsBuilder.Hosts;
+
+public static class HostExtensions
+{
+    public static object? ApplicationRun<TService, TArguments>(this IHost host, string[] args, string startupMethod) where TArguments : new()
+    {
+        var service = host.Services.GetService<TService>();
+        var serviceType = service!.GetType();
+        var method =
+            serviceType.GetMethod(startupMethod, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase)
+            ?? serviceType.GetMethod(startupMethod, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.IgnoreCase);
+        return method!.Invoke(service, new object?[] { ArgumentsBuilder.Parse<TArguments>(args) });
+    }
+
+    public static object? ApplicationRun<TService, TArguments>(this IHost host, string[] args) where TArguments : new()
+    {
+        var service = host.Services.GetService<TService>();
+        var serviceType = service!.GetType();
+        var method =
+            Enumerable.Concat(
+                serviceType.GetMethods(BindingFlags.Public | BindingFlags.Instance),
+                serviceType.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+            )
+            .FirstOrDefault(m => m.GetParameters().Select(p=>p.ParameterType).SequenceEqual(new[] { typeof(TArguments) }));
+        return method!.Invoke(service, new object?[] { ArgumentsBuilder.Parse<TArguments>(args) });
+    }
+}
+
