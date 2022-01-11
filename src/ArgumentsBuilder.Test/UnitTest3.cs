d44f0db0e7fd875d9628aab72356e3ca91e029d5
is12t <love.ms.is12t@hotmail.co.jp>
2022-01-12 01:18:18 +0900

    add AddCommandLineArguments

---
+++ src/ArgumentsBuilder.Test/UnitTest3.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NUnit.Framework;
+using Rksoftware.ArgumentsBuilder.Attributes;
+using Rksoftware.ArgumentsBuilder.Hosts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArgumentsBuilder.Test;
+internal class UnitTest3
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    record struct Arguments([Parameter] string? Aparam, [Parameter] string? Bparam, string? A, string? B, string? C, bool D, bool E) { }
+
+    interface IService { }
+
+    class Service : IService
+    {
+        public IConfiguration configuration;
+        public Service(IConfiguration configuration) => this.configuration = configuration;
+        public string Run(Arguments arguments) => arguments.ToString();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var args = new[] {
+            "aparam",
+            "-a",
+            "aopt",
+            "-d",
+            "-b",
+            "bopt",
+            "bparam",
+            "/c",
+            "copt",
+            };
+        using IHost host = new HostBuilder().ConfigureServices((_, services) => services.AddSingleton<IService, Service>())
+            .ConfigureAppConfiguration(config => config.AddCommandLineArguments<Arguments>(args))
+            .Build();
+        var config = (Microsoft.Extensions.Configuration.ConfigurationRoot)((Service)host.Services.GetService<IService>()!).configuration;
+        var d = config.Providers.Skip(4).FirstOrDefault();
+        var rsult = host.ApplicationRun<IService, Arguments>(args);
+        Assert.AreEqual(rsult, "Arguments { Aparam = aparam, Bparam = bparam, A = aopt, B = bopt, C = copt, D = True, E = False }");
+    }
+
+
+}
---
+++ src/ArgumentsBuilder/Hosts/ConfigurationExtendions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rksoftware.ArgumentsBuilder.Hosts;
+
+public static class ConfigurationExtendions
+{
+    public static IConfigurationBuilder AddCommandLineArguments<T>(this IConfigurationBuilder configurationBuilder, T arguments)
+    {
+        var type = typeof(T);
+        var args =
+        Enumerable.Concat(
+        type.GetFields().Select(f => (f.Name, f.GetValue(arguments)?.ToString()))
+        , type.GetProperties().Select(p => (p.Name, p.GetValue(arguments)?.ToString())))
+            .GroupBy(kv => kv.Name).Select(kv => ($"/{kv.Key}", kv.First().Item2)).SelectMany(kv => new string[] { kv.Item1, (kv.Item2) }).ToArray();
+        
+        return configurationBuilder.AddCommandLine(args);
+    }
+
+    public static IConfigurationBuilder AddCommandLineArguments<T>(this IConfigurationBuilder configurationBuilder, string[] args) where T:new() 
+    {
+        var arguments = ArgumentsBuilder.Parse<T>(args);
+
+        return configurationBuilder.AddCommandLineArguments(arguments);
+
+    }
+
+
+}
